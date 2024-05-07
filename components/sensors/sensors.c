db47ec5301d245a1d13d5004f3b083f85bf320ed
NaBsiiik <166906180+NaBsiiik@users.noreply.github.com>
2024-05-07 09:54:29 +0200

    Update sensors.c Readpin

--- components/sensors/sensors.c
+++ components/sensors/sensors.c
@@ -2,10 +2,16 @@
 
 bool sensors_obstacle_in_front()
 {
-    return false;
+    int prawy_przod = gpio_get_level(IR_FRONT_R);
+    int lewy_przod = gpio_get_level(IR_FRONT_L);
+    
+    return (prawy_przod || lewy_przod);
 }
 
 bool sensors_obstacle_on_side()
 {
-    return false;
+    int prawy_bok = gpio_get_level(IR_SIDE_R);
+    int lewy_bok = gpio_get_level(IR_SIDE_L);
+    
+    return (prawy_bok || lewy_bok);
 }
